aeefd55ef46d49d05d4e18caf563f37b5703d69e
USER <USER>
2016-08-31 17:28:02 +0900

    Serviceを読み出す・終了する部分を追記

--- android/app/src/main/java/nifty/intern/teamc/beaconreminder/TaskListActivity.java
+++ android/app/src/main/java/nifty/intern/teamc/beaconreminder/TaskListActivity.java
@@ -6,10 +6,12 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import nifty.intern.teamc.IbeaconReceiver.IbeaconReceiver;
 import nifty.intern.teamc.database.DatabaseManager;
 
 public class TaskListActivity extends AppCompatActivity {
@@ -20,6 +22,11 @@ public class TaskListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_task_list);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Log.d("TaskList", "FOOBAR");
+
+        //Bluetooth Initialize
+        startService(new Intent(TaskListActivity.this, IbeaconReceiver.class));
 
         //NCMB initialize
         DatabaseManager.initialize(this.getApplicationContext());
@@ -59,5 +66,11 @@ public class TaskListActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        stopService(new Intent(TaskListActivity.this, IbeaconReceiver.class));
     }
 }
