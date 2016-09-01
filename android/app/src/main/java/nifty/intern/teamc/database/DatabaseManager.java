4097d26a7002da58f44fc7d80dbf91bcd012f7e6
Shunji Makino <shunji.makino@gmail.com>
2016-09-01 12:38:21 +0900

    fix DatabaseManager.java

--- android/app/src/main/java/nifty/intern/teamc/database/DatabaseManager.java
+++ android/app/src/main/java/nifty/intern/teamc/database/DatabaseManager.java
@@ -1,20 +1,72 @@
 package nifty.intern.teamc.database;
 
 import android.content.Context;
+import android.os.AsyncTask;
+import android.provider.ContactsContract;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.nifty.cloud.mb.core.DoneCallback;
+import com.nifty.cloud.mb.core.FindCallback;
 import com.nifty.cloud.mb.core.NCMB;
 import com.nifty.cloud.mb.core.NCMBException;
 import com.nifty.cloud.mb.core.NCMBObject;
+import com.nifty.cloud.mb.core.NCMBQuery;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import nifty.intern.teamc.beaconreminder.TaskListActivity;
 
 /**
  * Created by USER on 2016/08/31.
  */
-public class DatabaseManager {
-    private static final String APP_KEY="APP_KE";
+public class DatabaseManager extends AsyncTask<Integer, Integer, List<String>>{
+
+    private static final String APP_KEY="APP_KEY";
     private static final String CLIENT_KEY="CLIENT_KEY";
-    public static void initialize(Context context) {
+
+    private Context context;
+    private ListView listView;
+
+    public DatabaseManager(Context context, ListView listView){
+        super();
+        this.context = context;
+        this.listView = listView;
+    }
+
+    public void initialize(Context context) {
         NCMB.initialize(context, APP_KEY, CLIENT_KEY);
+    }
+
+
+    public List<String> getAllTask(){
+        //TestClassを検索するためのNCMBQueryインスタンスを作成
+        NCMBQuery<NCMBObject> query = new NCMBQuery<>("TestClass");
+        //keyというフィールドがvalueとなっているデータを検索する条件を設定
+        query.whereExists("task");
+        try {
+            List<NCMBObject> results = query.find();
+            List<String> tasklist = new ArrayList<String>();
+            for(NCMBObject obj: results){
+                tasklist.add(obj.getString("task"));
+            }
+            return tasklist;
+        }catch(NCMBException e){
+            return new ArrayList<String>();
+        }
+    }
+
+    @Override
+    protected List<String> doInBackground(Integer... value) {
+        return  getAllTask();
+    }
+
+    @Override
+    protected void onPostExecute(List<String> result) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(context,
+                android.R.layout.simple_expandable_list_item_1, result);
+        listView.setAdapter(adapter);
     }
 
     public static void storeTask(String name){
